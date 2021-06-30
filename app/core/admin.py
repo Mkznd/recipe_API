68938a41633a37431f41bed47c4cca998995b366
Mkznd26 <yzundai2@gmail.com>
2021-06-30 20:51:34 +0300

    Added admin changing users(that fieldset is just...) and test for it

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
 # noinspection PyUnresolvedReferences
 from core import models
@@ -8,6 +9,14 @@ from core import models
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
 
 
 admin.site.register(models.User, UserAdmin)
